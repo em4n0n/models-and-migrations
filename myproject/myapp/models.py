ffdf7306c24d0b6ff95d12cfee90d5d20cc8330f
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-24 21:47:12 -0700

    Modify drink variable to drink_name

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Drinks(models.Model):
-    drink = models.CharField(max_length=200)
+    drink_name = models.CharField(max_length=200)
     price = models.IntegerField()
     
